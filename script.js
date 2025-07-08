942b6a96575fec6f64bf4c48a57451e1c22f0b4a
FullyDevi <fullyarts@gmail.com>
2025-07-08 20:10:01 -0300

    Adição de tarefas

---
+++ script.js
@@ -0,0 +1,20 @@
+const inputTarefa = document.getElementById("inputTarefa")
+const botaoAdicionarTarefa = document.getElementById("botaoAdicionarTarefa")
+const listaTarefas = document.getElementById("listaTarefas")
+const mensagemErro = document.getElementById("error")
+
+let tarefas = []
+
+botaoAdicionarTarefa.addEventListener("click", function () {
+    if (inputTarefa.value == "") {
+        mensagemErro.innerHTML = `Insira um valor no campo de Tarefa para adicionar uma tarefa`
+    } else {
+        mensagemErro.textContent = ""
+        const tarefa = document.createElement("li")
+        tarefa.textContent = inputTarefa.value
+        listaTarefas.appendChild(tarefa)
+
+        tarefas.push(inputTarefa.value)
+        inputTarefa.value = ""
+    }
+})
